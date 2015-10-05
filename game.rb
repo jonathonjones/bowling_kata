d5a5d3923dc80535c87e94725c48d95da7f8ea0f
Jonathon Jones <jonathon.jones@ge.com>
2015-10-04 23:15:59 -0400

    Simplify score logic by extracting frame_index variable

--- game.rb
+++ game.rb
@@ -13,10 +13,11 @@ class Game
   def score
     score = 0
     10.times do |frame|
-      first = @rolls[2*frame]
-      second = @rolls[2*frame + 1]
+      frame_index = frame * 2
+      first = @rolls[frame_index]
+      second = @rolls[frame_index + 1]
       if first + second == 10 #spare
-        score += 10 + @rolls[2*frame + 2]
+        score += 10 + @rolls[frame_index + 2]
       else
         score += first + second
       end
