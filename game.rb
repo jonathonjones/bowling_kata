8be2b7eb8394f13fdb0f53acf48a13c7f8d0ce9c
Jonathon Jones <jonathon.jones@ge.com>
2015-10-04 22:59:00 -0400

    Remove unnecessary @score variable

--- game.rb
+++ game.rb
@@ -1,13 +1,11 @@
 # Represent a game of bowling
 class Game
   def initialize
-    @score = 0
     @rolls = []
     @current_roll = 0
   end
 
   def roll(pins_down)
-    @score += pins_down
     @rolls[@current_roll] = pins_down
     @current_roll += 1
   end
