afd044904472dfc8d6401ef918933bf0ab0d5093
Jonathon Jones <jonathon.jones@ge.com>
2015-10-04 22:38:17 -0400

    Move roll_many to top

--- game_spec.rb
+++ game_spec.rb
@@ -1,6 +1,10 @@
 require './game'
 
 describe Game do
+  def roll_many(n, pins)
+    n.times { subject.roll(pins) }
+  end
+
   it 'should score 0 for all gutterball game' do
     roll_many(20, 0)
     expect(subject.score).to eq 0
@@ -9,9 +13,5 @@ describe Game do
   it 'should score 20 when one pin knocked down each time' do
     roll_many(20, 1)
     expect(subject.score).to eq 20
-  end
-
-  def roll_many(n, pins)
-    n.times { subject.roll(pins) }
   end
 end
