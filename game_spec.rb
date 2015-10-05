cd587ea500534bcec375b7f064b247430934b7cc
Jonathon Jones <jonathon.jones@ge.com>
2015-10-04 22:34:07 -0400

    Extract repeated loop

--- game_spec.rb
+++ game_spec.rb
@@ -4,12 +4,16 @@ describe Game do
   it 'should score 0 for all gutterball game' do
     n = 20
     pins = 0
-    n.times { subject.roll(pins) }
+    roll_many(n, pins)
     expect(subject.score).to eq 0
   end
 
   it 'should score 20 when one pin knocked down each time' do
     20.times { subject.roll(1) }
     expect(subject.score).to eq 20
+  end
+
+  def roll_many(n, pins)
+    n.times { subject.roll(pins) }
   end
 end
